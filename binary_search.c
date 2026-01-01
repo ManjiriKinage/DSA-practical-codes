c69f1888f72b84462d9ed96978a2ec829388ad79
ManjiriKinage <manjirikinage2005@gmail.com>
2026-01-01 22:35:01 +0530

    binary search recursion

---
+++ binary_search.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int  binarySearch(int a[],int low,int high,int key)
+{
+    if(low>high)
+        return -1;
+
+    int  mid= (low+high) /2;
+
+    if(a[mid]== key)
+        return mid;
+    else if ( key < a[mid])
+        return binarySearch(a,low,mid-1,key);
+    else
+        return binarySearch(a,mid+1,high,key);
+    
+}
+int main(){
+     int a[50], n, key, i, result;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter sorted elements:\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    result= binarySearch(a,0,n-1,key);
+
+    if(result==-1)
+        printf("Element not found\n");
+    else
+        printf("Element found at index %d\n", result);
+
+return 0;
+}
